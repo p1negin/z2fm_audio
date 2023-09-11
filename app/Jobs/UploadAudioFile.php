91421fd503dd07505b3f1a271f732a55fa1baa6f
p1negin <pinegin@proton.me>
2023-09-11 16:40:57 +0300

    up

--- app/Jobs/UploadAudioFile.php
+++ app/Jobs/UploadAudioFile.php
@@ -15,10 +15,11 @@ class UploadAudioFile implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected array $proxy = [
-        'BhUMZP:5nGtMT@91.198.208.158:8000',
-        'Ds2zf9:j6uEKs@194.62.67.101:8000',
-        'k3gBfU:Ej4p7j@45.133.32.148:8000',
-        '4shetz:3YYN0S@45.133.32.245:8000',
+        //'BhUMZP:5nGtMT@91.198.208.158:8000',
+        //'Ds2zf9:j6uEKs@194.62.67.101:8000',
+        //'k3gBfU:Ej4p7j@45.133.32.148:8000',
+        //'4shetz:3YYN0S@45.133.32.245:8000',
+        'jqxzlzib:dv5xij6yy8ov@206.41.164.21:6320'
     ];
 
     /**
@@ -39,7 +40,7 @@ class UploadAudioFile implements ShouldQueue
 
         $client = new Client([
             'verify' => false,
-            'proxy' => 'https://' . $proxy,
+            //'proxy' => 'socks5://' . $proxy,
         ]);
         $contents = $client->get('https://z3.fm/download/' . $this->id)->getBody()->getContents();
         if($contents) {
