aaabeca370d3580ad5b2d2e75e56cf0a37284416
p1negin <pinegin@proton.me>
2023-09-11 13:31:33 +0300

    up

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Storage;
 
 class PlayController extends Controller
 {
     public function index(int $id)
     {
-        dd(file_get_contents('https://z3.fm/download/' . $id));
+
         if(!Storage::exists('audio_files/' .$id . '.mp3')) {
-            if(Storage::put('audio_files/' .$id . '.mp3', file_get_contents('https://z3.fm/download/' . $id))) {
-                return response()->file(Storage::path('audio_files/' .$id . '.mp3'), ['Content-Type' => 'audio/mp3']);
+            $client = new Client();
+            $contents = $client->get('https://z3.fm/download/' . $id)->getBody()->getContents();
+            if (Storage::put('audio_files/' . $id . '.mp3', $contents)) {
+                return response()->file(Storage::path('audio_files/' . $id . '.mp3'), ['Content-Type' => 'audio/mp3']);
             }
         }
         return response()->file(Storage::path('audio_files/' .$id . '.mp3'), ['Content-Type' => 'audio/mp3']);
