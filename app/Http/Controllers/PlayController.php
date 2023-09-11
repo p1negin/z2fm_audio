fc605cca398885d43ea04c56b3dee2703775431e
p1negin <pinegin@proton.me>
2023-09-11 13:37:39 +0300

    up

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 
 class PlayController extends Controller
@@ -10,13 +12,39 @@ class PlayController extends Controller
     public function index(int $id)
     {
 
-        if(!Storage::exists('audio_files/' .$id . '.mp3')) {
-            $client = new Client();
+        $remoteFileUrl = 'https://z3.fm/ajax/inc/' . $id; // Замените на URL удаленного файла
+        $response = Http::get($remoteFileUrl);
+        if ($response->ok()) {
+            $fileContents = $response->body();
+            $mime = $response->header('Content-Type');
+
+            $response = new Response($fileContents);
+            return $response->header('Content-Type', $mime);
+        } else {
+            abort(404);
+        }
+
+
+
+       /* if(!Storage::exists('audio_files/' .$id . '.mp3')) {
+            $remoteFileUrl = 'https://z3.fm/download/' . $id; // Замените на URL удаленного файла
+            $response = Http::get($remoteFileUrl);
+            if ($response->ok()) {
+                $fileContents = $response->body();
+                $mime = $response->header('Content-Type');
+
+                $response = new Response($fileContents);
+                return $response->header('Content-Type', $mime);
+            } else {
+                abort(404);
+            }
+
+
             $contents = $client->get('https://z3.fm/download/' . $id)->getBody()->getContents();
             if (Storage::put('audio_files/' . $id . '.mp3', $contents)) {
                 return response()->file(Storage::path('audio_files/' . $id . '.mp3'), ['Content-Type' => 'audio/mp3']);
             }
         }
-        return response()->file(Storage::path('audio_files/' .$id . '.mp3'), ['Content-Type' => 'audio/mp3']);
+        return response()->file(Storage::path('audio_files/' .$id . '.mp3'), ['Content-Type' => 'audio/mp3']);*/
     }
 }
