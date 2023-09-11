cd16e100849f82df42b154e9bdf292cd63f13935
p1negin <pinegin@proton.me>
2023-09-11 14:18:15 +0300

    up

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -7,12 +7,27 @@ use Illuminate\Support\Facades\Storage;
 
 class PlayController extends Controller
 {
+    protected array $proxy = [
+        'BhUMZP:5nGtMT@91.198.208.158:8000',
+        'Ds2zf9:j6uEKs@194.62.67.101:8000',
+        'k3gBfU:Ej4p7j@45.133.32.148:8000',
+        '4shetz:3YYN0S@45.133.32.245:8000',
+    ];
+
+
     public function index(int $id)
     {
 
-        if(!Storage::exists('audio_files/' .$id . '.mp3')) {
+
+        if (!Storage::exists('audio_files/' . $id . '.mp3')) {
+
+            $i = array_rand($this->proxy);
+            $proxy = $this->proxy[$i];
+
             $client = new Client([
-                'verify' => false
+                'verify' => false,
+                'proxy' => 'socks5://' . $proxy,
+
             ]);
             $contents = $client->get('https://z3.fm/download/' . $id)->getBody()->getContents();
             if (Storage::put('audio_files/' . $id . '.mp3', $contents)) {
