a32bbc68fce0810728255f0a8ad0b09e40d2be98
p1negin <pinegin@proton.me>
2023-09-11 14:42:05 +0300

    up

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Jobs\UploadAudioFile;
 use Illuminate\Support\Facades\Storage;
 
 class PlayController extends Controller
@@ -11,7 +10,7 @@ class PlayController extends Controller
     public function index(int $id)
     {
         if (!Storage::exists('audio_files/' . $id . '.mp3')) {
-           // UploadAudioFile::dispatch($id);
+            UploadAudioFile::dispatch($id);
             return redirect('https://z3.fm/download/' . $id, 301);
         }
         return response()->file(Storage::path('audio_files/' .$id . '.mp3'), ['Content-Type' => 'audio/mp3']);
--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'sqlite'),
+    'default' => env('QUEUE_CONNECTION', 'redis'),
 
     /*
     |--------------------------------------------------------------------------
